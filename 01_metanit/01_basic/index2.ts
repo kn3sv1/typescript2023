27fe7fd8afa111e44a76556534d8e8f476293367
kn3sv1 <kn3sv1@example.com>
2023-02-25 16:02:03 +0200

    example basic2

--- 01_metanit/01_basic/index2.ts
+++ 01_metanit/01_basic/index2.ts
@@ -2,5 +2,5 @@ function min (x: number, y: number): number {
     return x + y;
 };
 
-let d6 = min(10,61);
+let d6 = min(10,62);
 console.log(d6);
---
+++ 01_metanit/03_basic2/index.ts
@@ -0,0 +1,6 @@
+function sum (x: number, y: number): number {
+    return x + y;
+};
+
+let d3 = sum(10, 6);
+console.log(d3);
